a1a469bb04477b8c39d868a9ee6236b6d12fd36b
ma-mani <mani.mashayekhi3@gmail.com>
2024-01-17 13:38:42 +0100

    fix: Use URLSearchParams to encode

--- pages/api/movies/index.js
+++ pages/api/movies/index.js
@@ -1,10 +1,15 @@
 export default async function handler(request, response) {
   const { search } = request.query;
-  const api_KEY = process.env.tmdbApiKey;
+  const api_key = process.env.tmdbApiKey;
+
+  console.log(new URLSearchParams({ query: search, api_key }));
 
   if (request.method === "GET") {
     const result = await fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${api_KEY}`
+      `https://api.themoviedb.org/3/search/movie?${new URLSearchParams({
+        query: search,
+        api_key,
+      })}`
     );
     const data = await result.json();
 
